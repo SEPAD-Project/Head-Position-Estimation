a3cd32cd9fd1b8439aa8170d39f82e20802ebbbe
parsasafaie <parsasafaie.2568@proton.me>
2025-02-13 13:20:12 +0330

    refine yaw and pitch formula

--- yaw_pitch/func_yaw_pitch.py
+++ yaw_pitch/func_yaw_pitch.py
@@ -55,23 +55,18 @@ def yaw_pitch(image_path=None, frame=None):
             chin = landmarks[152]  # Chin
             left_eye_outer = landmarks[33]  # Outer corner of the left eye
             right_eye_outer = landmarks[263]  # Outer corner of the right eye
+            nasion = landmarks[168] # Nasion
 
             # Calculate depth estimation using the Z coordinate of the nose
             depth = abs(nose_tip.z) * 100
 
-            # Convert normalized coordinates (0-1) to pixel values
-            nose_tip = (int(nose_tip.x * image_width), int(nose_tip.y * image_height))
-            chin = (int(chin.x * image_width), int(chin.y * image_height))
-            left_eye_outer = (int(left_eye_outer.x * image_width), int(left_eye_outer.y * image_height))
-            right_eye_outer = (int(right_eye_outer.x * image_width), int(right_eye_outer.y * image_height))
-
             # Compute yaw (horizontal head rotation)
             # Yaw is the horizontal distance of the nose from the midpoint of the eyes
-            yaw = (left_eye_outer[0] + right_eye_outer[0]) / 2 - nose_tip[0]
+            yaw = (right_eye_outer.x-nose_tip.x) - (nose_tip.x-left_eye_outer.x)
 
             # Compute pitch (vertical head rotation)
             # Pitch is the vertical distance between the chin and nose tip, offset by 90
-            pitch = (chin[1] - nose_tip[1]) - 90
+            pitch = (chin.y - nose_tip.y) - (nose_tip.y - nasion.y)
 
             return (yaw, pitch, depth)  # Return yaw and pitch if successful
         
