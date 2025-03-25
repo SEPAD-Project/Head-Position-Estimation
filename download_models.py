4f69b6e8ca527b4106f6cc293d5fe232c4ede5de
parsasafaie <parsasafaie.2568@proton.me>
2025-03-25 12:58:59 +0330

    update models saving path

--- download_models.py
+++ download_models.py
@@ -1,10 +1,10 @@
 from urllib.request import urlretrieve
 import os
 
-OPENCV_FACE_DETECTOR_PATH = r"c:\\sap-program\\opencv\\haarcascade_frontalface_default.xml"
-OPENCV_FACE_RECOGNIZER_PATH = r"c:\\sap-program\\opencv\\face_recognition_sface_2021dec.onnx"
+OPENCV_FACE_DETECTOR_PATH = r"c:\\sap-project\\opencv\\haarcascade_frontalface_default.xml"
+OPENCV_FACE_RECOGNIZER_PATH = r"c:\\sap-project\\opencv\\face_recognition_sface_2021dec.onnx"
 
-os.makedirs(r"c:\\sap-program\\opencv")
+os.makedirs(r"c:\\sap-project\\opencv")
 
 def download():
     try:
